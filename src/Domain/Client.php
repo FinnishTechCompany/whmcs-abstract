ba2628105660f54aab5375f1f3be519e99692b03
Niko Granö <niko@ironlions.fi>
2019-10-08 16:42:43 +0300

    Fix license headers

--- src/Domain/Client.php
+++ src/Domain/Client.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 /**
  *
- * WHMCS Gateway Fees 2019 — NOTICE OF LICENSE
+ * WHMCS Abstract 2019 — NOTICE OF LICENSE
  * This source file is released under commercial license by copyright holders.
+ * Please see LICENSE file for more specific licensing terms.
  * @copyright 2017-2019 (c) Niko Granö (https://granö.fi)
  * @copyright 2014-2019 (c) IronLions (https://ironlions.fi)
  *
